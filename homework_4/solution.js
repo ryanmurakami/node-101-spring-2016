5a3010bdcd5bdc9adb62cfb6183c88966bd079d3
Ryan Lewis <rylewis@expedia.com>
2016-05-19 12:39:20 -0700

    homework 4 solution

--- homework_4/solution.js
+++ homework_4/solution.js
@@ -1,3 +1,31 @@
-module.exports = function (jsonString) {
+var Hotel = require('./Hotel.js');
 
+module.exports = function (jsonString) {
+  var responseObj,
+    returnArray = [];
+
+  // try to parse string into JavaScript
+  try {
+    responseObj = JSON.parse(jsonString);
+  } catch (err) {
+    throw new Error('BAD JSON');
+  }
+
+  // check if responseObj is falsy or hotelList doesn't exist or is empty
+  if (!responseObj || !responseObj.hotelList || !responseObj.hotelList.length) {
+    return [];
+  }
+
+  // loop through hotel list and create Hotel objects
+  for (var ix in responseObj.hotelList) {
+    returnArray.push(new Hotel(responseObj.hotelList[ix]));
+  }
+
+  // // another way to do it using the Array.map() function
+  // 
+  // returnArray = responseObj.hotelList.map(function (obj) {
+  //   return new Hotel(obj);
+  // });
+
+  return returnArray;
 };
