8e43133f1a24412a5012396097d28c869d7c7761
Ryan Lewis <rylewis@expedia.com>
2016-05-19 13:49:32 -0700

    merge

--- homework_3/solution.js
+++ homework_3/solution.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-var colors = require("colors");
-
-var mathFuns = {
-    "+" : function(x, y) { return x + y; },
-    "-" : function(x, y) { return x - y; },
-    "x" : function(x, y) { return x * y; },
-    "/" : function(x, y) { return x / y; },
-    "^" : function(x, y) { return Math.pow(x, y); }
-}
-
-function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-module.exports = function (equation) {
-    var ops = equation.split(" ");
-    var result;
-
-    if (ops.length < 2) {
-        result = Number(ops[0]);
-    } else {
-        var curVal = Number(ops[0]);
-        var curOp = ops[1];
-        for (var i = 2; i < ops.length; i++) {
-            if (isNumber(ops[i])) {
-                curVal = mathFuns[curOp](curVal, Number(ops[i]));
-            } else {
-                curOp = ops[i];
-            }
-        }
-        result = curVal;
-    }
-
-    result = result.toString();
-
-    if (result > 0) {
-        return result.green;
-    } else if (result == 0) {
-        return result.yellow;
-    } else {
-        return result.red;
-    }
-}
-=======
 var colors = require('colors');
 
 // calculator program
@@ -63,10 +18,10 @@ function calculate(inputString) {
 
     // loop until no more operators
     while (operators.length > 0) {
-        
+
         // save the current operator
         var op = operators.shift();
-        
+
         // switch based on operator
         switch (op) {
             case '+':
@@ -86,9 +41,9 @@ function calculate(inputString) {
                 break;
         }
     }
-    
+
     var result = operands[0] + ''; //coerces to string type
-    
+
     // colors
     if (operands[0] < 0) {
         return result.red;
@@ -100,4 +55,3 @@ function calculate(inputString) {
 }
 
 module.exports = calculate;
->>>>>>> master
