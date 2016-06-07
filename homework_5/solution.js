70f67c652827bfef70fcd0b005c10b7a7b2a4d56
Ryan Lewis <rylewis@expedia.com>
2016-06-07 14:51:40 -0700

    examples

--- homework_5/solution.js
+++ homework_5/solution.js
@@ -1,17 +1,20 @@
 var getSearchResults = require('./get-search-results.js');
 var makeHotelList = require('../homework_4/solution.js');
 
-module.exports.run = function(resultCallback) {
-  var json = '';
-  var makeResponse = function(stringJson) {
+function run (resultCallback) {
+
+  var makeResponse = function (stringJson) {
     try {
-      var json = makeHotelList(stringJson);
-      resultCallback(null, json);
-    }
-    catch(err) {
-      resultCallback(err, null);
+      var jsObj = makeHotelList(stringJson);
+      resultCallback(null, jsObj);
+    } catch (err) {
+      resultCallback(err);
     }
   };
 
   getSearchResults(makeResponse);
+};
+
+module.exports = {
+  run: run
 };
